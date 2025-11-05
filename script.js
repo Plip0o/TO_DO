6ced08244dd898066ba9c2eae9308ff7f298004d
Plip0o <phelipealcantarasellera@gmail.com>
2025-11-04 22:29:41 -0300

    ,

--- script.js
+++ script.js
@@ -2,71 +2,70 @@
 let taskInput = document.getElementById('taskInput');
 const btnAdd = document.getElementById('btnAdd');
 let task = document.getElementById('task');
- 
+
 //cria uma var que vai servir como um contador 
 let taskIdCounter = 0
 
 function addItem() {
-    if(taskInput.value ==""){
+    if (taskInput.value == "") {
         alert('digite uma nova tarefa');
         taskInput.focus();
     } else {
-// CREATE 
+        taskIdCounter++
+        // CREATE 
         //cria um elemento, um li
-        let li = document.createElement('li'); 
+        let li = document.createElement('li');
         //adc o valor do imput dentro da li
         li.innerHTML = taskInput.value;
-        
+
         //cria uma var e coloca o contador dentro
         const taskId = `task-${taskIdCounter}`
-        
+
         //coloca a var com o contador dentro da li como um id usando o dataset
         //que é um objeto 
         li.dataset.id = taskId
 
-    // criando as images que vao servir como btn
+        // criando as images que vao servir como btn
 
-        //cria um elemento novo, uma imagem
-        const visibility = document.createElement('img')
-        //mostra qual qual img vai ser
-        visibility.src = 'img/visibility.png';
-        //coloca um id na img 
-        visibility.id = `visibility-${taskId}`; 
-        
-        const edit = document.createElement('img')
-        edit.src = 'img/edit.png';
-        edit.id = `edit-${taskId}` 
+        const btnsImg = (taskId) => {
 
-        const cancel = document.createElement('img')
-        cancel.src = 'img/cancel.png'
-        cancel.id = `cancel-${taskId}`
+            const visibility = document.createElement('img');
+            visibility.src = 'img/visibility.png';
+            visibility.id = `visibility-${taskId}`;
+            visibility.addEventListener('click', visibilityfunction);
+            
+            const edit = document.createElement('img');
+            edit.src = 'img/edit.png';
+            edit.id = `edit-${taskId}`;
+            edit.addEventListener('click', editfunction);
+            
+            const cancel = document.createElement('img');
+            cancel.src = 'img/cancel.png';
+            cancel.id = `cancel-${taskId}`;
+            cancel.addEventListener('click',cancelfunction);
 
-        //cria um elemento novo, no caso uma span
-        //usei um <span> ao inves de uma <div> por conta do box-inline-level
-        //(agrupar as imgs pra facilitar a estilização)
-        const iconsContainer = document.createElement('span')
-        //adc uma class na span
-        iconsContainer.classList.add('imgsIcons')
+            const iconsContainer = document.createElement('span');
+            //adc uma class na span
+            iconsContainer.classList.add('imgsIcons');
 
-        //colocando as imgs dentro da span
-        iconsContainer.appendChild(visibility)
-        iconsContainer.appendChild(edit)
-        iconsContainer.appendChild(cancel)
+            iconsContainer.append(visibility, edit, cancel)
 
-        //mostrando a <span class = imgsIcons>
-        li.appendChild(iconsContainer)
-       
+            return iconsContainer;
+            
+        }
+
+        li.appendChild(btnsImg (taskId));
+
         //cria o a li dentro do <ul id="task">
         task.appendChild(li);
 
         //coloca o valor do input como vazio novamente e coloca o cursor nele
         taskInput.value = "";
         taskInput.focus();
-
-// READ
-        
+        // READ
+        // CRIAR MODAL        
         //diz pro html que quando eu clicar no visibility ele vai fazer isso:
-        visibility.addEventListener('click',function(){
+        function visibilityfunction() {
             //pega o id do visibility e coloca dentro da var
             const visibilityId = this.id;
             //separa a var em duas pega o contador, e coloca na var taskId
@@ -94,15 +93,116 @@ function addItem() {
 
             modal.showModal();
 
-            close.addEventListener('click',function(){
-                modal.close()
+            // DELETE MODAL
+            close.addEventListener('click', function () {
+                //pega o item
+                //pega o id do item
+                const closeId = this.id;
+                const cId = closeId.split(`close-`)[1]
+
+                //seleciona o item pelo id
+                const deleteModal = document.querySelector(`#modal-${cId}`);
+
+                //fecha o modal
+                modal.close();
+
+                //apaga o item
+                deleteModal.remove();
+
             })
 
-        })
+        }
 
-// UPDATE
-// DELETE
-        cancel.addEventListener('click',function(){
+        // UPDATE
+        function editfunction() {
+            //cria um dialog
+            const modal = document.createElement('dialog')
+
+            //coloca um id no modal
+            const modalId = this.id.split(`task-`)[1]
+            modal.id = `modal-${modalId}`
+
+            //cria um input 
+            editTask = document.createElement('input')
+
+            //editar placeholder
+            const valortask = this.id.split(`edit-`)[1]
+            const oldTask = document.querySelector(`[data-id="${valortask}"]`);
+            let oldTaskvalue = oldTask.textContent
+            editTask.placeholder = oldTaskvalue
+            //coloca type
+            editTask.type = "text"
+            //coloca id
+            const inputId = this.id.split(`task-`)[1]
+            editTask.id = `newTask-${inputId}`
+
+            //coloca o input dentro do modal
+            modal.appendChild(editTask)
+
+            //cria os btns de sair e confirmar
+            const btnCloseEdit = document.createElement('img')
+            btnCloseEdit.src = "img/close.png"
+            const btncheck = document.createElement('img')
+            btncheck.src = "img/check.png"
+
+            //coloca id nos btns
+            const closeEditId = this.id.split(`edit-task-`)[1]
+            btnCloseEdit.id = `closeEdit-${closeEditId}`
+
+            const btncheckId = this.id.split(`edit-task-`)[1]
+            btncheck.id = `check-${btncheckId}`
+
+            //cria um container pros btns
+            const btnscontainer = document.createElement('span')
+            modal.appendChild(btnscontainer)
+
+            //coloca os btns dentro do container
+            btnscontainer.appendChild(btncheck)
+            btnscontainer.appendChild(btnCloseEdit)
+
+            //coloca o modal dentro do bod
+            document.body.appendChild(modal)
+
+            //abre o modal
+            modal.showModal()
+
+            btnCloseEdit.addEventListener('click', function () {
+                //pega o id do btn
+                const btnCId = this.id.split(`closeEdit-`)[1]
+                // seleciona o modal modal
+                const removeModal = document.querySelector(`#modal-${btnCId}`);
+
+                //apaga e remove modal
+                removeModal.close();
+                removeModal.remove();
+            })
+           
+            btncheck.addEventListener('click', function () {
+
+
+                //pega o valor do input
+                const inputcont = this.id.split('check-')[1]
+                const newTaskId = `newTask-${inputcont}`
+                const newTask = document.getElementById(newTaskId);
+                const newTaskValue = newTask.value
+
+                if (newTaskValue == "") {
+
+                    window.alert("digite algo!")
+
+                } else {
+                    let oldTask = document.querySelector(`[data-id="task-${inputcont}"]`);
+                    
+                    oldTask.textContent = (newTaskValue);
+                    oldTask.appendChild(btnsImg (taskId));
+
+                    modal.close();
+                }
+
+            })
+        }
+        // DELETE TASK
+        function cancelfunction() {
 
             //pega o id da img 
             const cancelId = this.id;
@@ -114,13 +214,7 @@ function addItem() {
             //remove a li
             liToRemove.remove();
 
-        })
-
-        
-        //faz o contador funcionar, adc +1
-        taskIdCounter ++
+        }
+     
     }
 }
-
-        // li.onclick = () => editTask (taskId)
-        // console.log("oi")
